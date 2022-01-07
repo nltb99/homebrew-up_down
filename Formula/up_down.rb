eee073a52efd684985018970a5689bafc276eedc
nltbao <tienbao.nguyen@mafc.com.vn>
2022-01-07 11:36:07 +0700

    First release

--- Formula/up_down.rb
+++ Formula/up_down.rb
@@ -1,15 +1,15 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class UpDown < Formula
+class Updown < Formula
   desc "Cross-platform Text Expander written in Rust"
   homepage "https://github.com/nltb99/up_down"
-  url "https://github.com/nltb99/up.down/releases/download/v0.1.0/up_down.tar.gz"
-  sha256 "3e5d998781e006f0bc0eccf9e6c14dfc042845dedc3bcdbc164841a6488bd836"
+  url "https://github.com/nltb99/up_down/releases/download/v0.1.0/up_down.tar.gz"
+  sha256 "e002a7e53c510aa32f30e81ae47eb4e64c3cb094a6ec43213b87189a444ce500"
   version "0.1.0"
 
   def install
-    bin.install "a"
+    bin.install "up_down"
   end
 end
 
