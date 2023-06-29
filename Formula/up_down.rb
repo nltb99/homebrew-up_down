e5c0b569cd2d3709d2810370ef1336d891fa2a3f
nltb99 <nguyenletienbao99@gmail.com>
2023-06-29 21:09:25 +0700

    update version

--- Formula/up_down.rb
+++ Formula/up_down.rb
@@ -4,8 +4,8 @@ class UpDown < Formula
   desc "Console game implemented in C++ using ncurses as an application programming interface"
   homepage "https://github.com/nltb99/up_down"
   url "https://github.com/nltb99/up_down/releases/download/v0.2.0/up_down.tar.gz"
-  sha256 "022e4f3b59a24847a732cf2b733ee463e743fcd216c4e2567246aa22fff988cd"
-  version "0.1.0"
+  sha256 "dece1239cd6ef89ad0a8c00740c5ddf3c1b88a9f31d9cc48978cf0ad2b0cfc82"
+  version "0.2.0"
 
   def install
     bin.install "up_down"
